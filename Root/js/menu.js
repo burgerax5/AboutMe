7f18ae56b61dd1be4337d12a5558a6ea1862d778
burgerax5 <93895662+burgerax5@users.noreply.github.com>
2022-04-21 14:17:15 +1200

    Collapsible Sidebar For Subpages
    The profile, projects and contact page now have a collapsible sidebar which can be used to navigate the website.

--- Root/js/menu.js
+++ Root/js/menu.js
@@ -1,12 +1,16 @@
 const menuBtn = document.querySelector('.menu');
+const sidebar = document.querySelector('.sidebar')
+
 let menuOpen = false;
 menuBtn.addEventListener('click', () => {
     if(!menuOpen){
         menuBtn.classList.add('open');
         menuOpen = true;
+        sidebar.style.display = 'block';
     }
     else{
         menuBtn.classList.remove('open');
         menuOpen = false;
+        sidebar.style.display = 'none';
     }
 });
