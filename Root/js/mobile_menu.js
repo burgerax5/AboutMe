67a5993817bd3c102355cfe456bea47ce121a87e
burgerax5 <93895662+burgerax5@users.noreply.github.com>
2022-04-25 00:52:17 +1200

    Full Redesign
    I completely redesigned the website. I didn't add any new html files but I completely changed the HTML in those files in order for me to do the redesign.
    
    For the redesign, I tweaked the layout of the pages as well as the colour scheme and aesthetic. I wanted my website to have a low-key Genshin Impact theme to it.

---
+++ Root/js/mobile_menu.js
@@ -0,0 +1,6 @@
+const navBtn = document.querySelector('.nav-m-btn');
+const navMenu = document.querySelector('.nav-m-overlay')
+
+navBtn.addEventListener('click', () => {
+    navMenu.classList.toggle('open');
+});
