c1b417dc4ab05d9fe9e5f684f577d38e6c5e24df
burgerax5 <93895662+burgerax5@users.noreply.github.com>
2022-04-27 20:16:13 +1200

    Redesigned Subpages
    I redesigned my profile and projects page.
    
    For the  profile page, rather than having all the content on top of one another on desktop resolution, the content will be split into 2 parts, LHS is the profile picture, basic info such as name, birthday & occupation. RHS is the more descriptive side with a list of my favourite games, music artists and anime.
    
    I changed the projects page to have a 3-column grid to display the items. Still a WIP.

--- Root/js/menu.js
+++ Root/js/menu.js
@@ -1,6 +1,3 @@
-const navBtn = document.querySelector('.nav-m-btn');
-const navMenu = document.querySelector('.nav-m-overlay');
-
 const hoverHome = document.getElementById('home-link')
 const homeText = document.getElementById('expand-home');
 
@@ -13,35 +10,40 @@ const projectsText = document.getElementById('expand-projects');
 const hoverContact = document.getElementById('contact-link')
 const contactText = document.getElementById('expand-contact');
 
-navBtn.addEventListener('click', () => {
-    navMenu.classList.toggle('open');
-});
 /*Hover Home*/
 hoverHome.addEventListener('mouseenter', () => {
+    homeText.classList.remove('close');
     homeText.classList.add('open');
 });
 hoverHome.addEventListener('mouseleave', () => {
+    homeText.classList.add('close');
     homeText.classList.remove('open');
 });
 /*Hover Profile*/
 hoverProfile.addEventListener('mouseenter', () => {
+    profileText.classList.remove('close');
     profileText.classList.add('open');
 });
 hoverProfile.addEventListener('mouseleave', () => {
+    profileText.classList.add('close');
     profileText.classList.remove('open');
 });
 /*Hover Projects*/
 hoverProjects.addEventListener('mouseenter', () => {
+    projectsText.classList.remove('close');
     projectsText.classList.add('open');
 }); 
 hoverProjects.addEventListener('mouseleave', () => {
+    projectsText.classList.add('close');
     projectsText.classList.remove('open');
 });
 /*Hover Contact*/
 hoverContact.addEventListener('mouseenter', () => {
+    contactText.classList.remove('close');
     contactText.classList.add('open');
 });
 hoverContact.addEventListener('mouseleave', () => {
+    contactText.classList.add('close');
     contactText.classList.remove('open');
 });
 
---
+++ Root/js/projects.js
@@ -0,0 +1,3 @@
+const itemLink = document.querySelector('.project-item');
+
+
