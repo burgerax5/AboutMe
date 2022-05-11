221e15b748a01bf47ca6bf058e78287cf425f046
burgerax5 <93895662+burgerax5@users.noreply.github.com>
2022-05-11 22:00:46 +1200

    Contact Page Improved
    When a contact item is clicked, a modal will popup in the middle of the screen asking if the user wants to go to another website with a yes/no button.
    
    Still need to fix the mobile-view for the contact-items and modal as it gets squished.

--- Root/js/contact.js
+++ Root/js/contact.js
@@ -1,13 +1,22 @@
 const onClick = function(){
     let order = ['steamLink', 'malLink', 'githubLink', 'leetcodeLink', 'discordLink']
     let links = ['https://steamcommunity.com/id/burgerax3', 'https://myanimelist.net/profile/burgerax3', 'https://github.com/burgerax5', 'https://leetcode.com/burgerax3/', 'https://discord.gg/D9JYv5e']
-    
+    modal.style.visibility = 'visible';
+
     for(let i = 0; i < order.length; i++){
         if (order[i] == this.id){
-            if (confirm('Do you wish to go to another website?') == true){
-                location.href = links[i];
-            }
+            modalLink.innerHTML = links[i]
+            break;
         }
+    }
+}
+
+const confirmClick = function(){
+    if(this.id == 'yesBtn'){
+        location.href = modalLink.innerHTML;
+    }
+    else{
+        modal.style.visibility = 'hidden';
     }
 }
 
@@ -16,3 +25,7 @@ document.getElementById('malLink').onclick = onClick;
 document.getElementById('githubLink').onclick = onClick;
 document.getElementById('leetcodeLink').onclick = onClick;
 document.getElementById('discordLink').onclick = onClick;
+const modal = document.getElementById('contactModal');
+let modalLink = document.getElementById('modalLink');
+document.getElementById('yesBtn').onclick = confirmClick;
+document.getElementById('noBtn').onclick = confirmClick;
